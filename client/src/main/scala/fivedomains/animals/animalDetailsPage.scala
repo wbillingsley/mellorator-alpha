8eec37dd914a3c987e47f57a0381c3ff16591d0a
William Billingsley <wbilling@une.edu.au>
2023-08-15 16:35:32 +1000

    Push changes from desktop around animal details page

--- client/src/main/scala/fivedomains/animals/animalDetailsPage.scala
+++ client/src/main/scala/fivedomains/animals/animalDetailsPage.scala
@@ -14,7 +14,6 @@ def assessmentQuantumStats(a:Animal, surveys:Seq[Assessment]) =
     // TODO - check if many of the surveys have been with low confidence
     <.div(^.cls := nakedParaMargins,
         <.p(
-            s"There's ${surveys.length} assessments on file for ${a.name}. ",
             f"Overall confidence across the surveys was ${surveys.overallConfidence * 100}%.0f%%."
         ),
         <.p(
@@ -39,32 +38,10 @@ def animalDetailsPage(aId:AnimalId) =
             <.label(^.cls := (animalName), a.name)
         ),
 
-        assessmentQuantumStats(a, DataStore.surveysFor(a)),
+        
 
-        // Widget for surveys
-        if surveys.isEmpty then 
-            assessments.sevenBox(Map.empty)(^.style := "")
-        else if surveys.length == 1 then
-            val s = surveys.head
-            <.div(^.style := "margin: 1.5em;",
-                <.div(^.style := "text-align: center;",
-                    assessments.scoreText7(s)
-                )
-            )
-        else
-            <.div(^.style := "margin: 1.5em;",
-                <.div(^.style := "text-align: center;",
-                    assessments.sparkTrend7(surveys)
-                )
-            ),
+        SurveySelectWidget(a, surveys)
 
-        <.div(^.style := s"padding: 5px 1em; background: ${Domain.Mental.color}",
-                        <.label(^.style := "color: white", Domain.Mental.toString),
-        ),
-
-        <.div(^.style := "margin: 1.5em;",
-            <.p("...trend of last surveys...?")
-        ),
 
         /*for d <- Seq(Domain.Nutrition, Domain.Environment, Domain.Health, Domain.Behaviour) yield <.div(
             <.div(^.style := s"padding: 5px 1em; background: ${domainColour(d)}",
@@ -80,3 +57,67 @@ def animalDetailsPage(aId:AnimalId) =
 
 
     )
+
+
+case class SurveySelectWidget(animal:Animal, surveys:Seq[Assessment]) extends DHtmlComponent {
+
+    val max = surveys.length
+    val number = stateVariable(max)
+
+    def subset = surveys.slice(surveys.length - number.value, surveys.length)
+
+    override def render = <.div(
+
+        <.div(^.cls := alignCentreStyle,    
+            <.p(s"Showing most recent ${number.value} of $max surveys"),
+            <.p(
+                <.input(
+                    ^.attr("type") := "range", ^.attr("min") := 1, ^.attr("max") := max,
+                    ^.prop("value") := number.value, ^.on.input ==> { e => for v <- e.inputValue do number.value = v.toInt }
+                ),
+            ),
+        ),
+
+        surveySummary(animal, subset)
+    )
+
+}
+
+
+def surveySummary(animal:Animal, surveys:Seq[Assessment]) = <.div(
+    // Widget for surveys
+    if surveys.isEmpty then 
+        assessments.sevenBox(Map.empty)(^.style := "")
+    else if surveys.length == 1 then
+        val s = surveys.head
+        <.div(^.style := "margin: 1.5em;",
+            <.div(^.style := "text-align: center;",
+                assessments.scoreText7(s)
+            )
+        )
+    else
+        <.div(^.style := "margin: 1.5em;",
+            <.div(^.style := "text-align: center;",
+                assessments.colouredSparkTrend7(surveys)
+            )
+        ),
+
+    <.div(^.cls := nakedParaMargins,
+        assessmentQuantumStats(animal, surveys),
+        warningFlags(surveys),
+        lowestRatingAdvice(surveys), 
+        confidenceAdvice(surveys)   
+    )
+)
+
+def warningFlags(surveys:Seq[Assessment]) = <.div(
+    <.p("Spot for warning flags we detect from these assessments...")
+)
+
+def lowestRatingAdvice(surveys:Seq[Assessment]) = <.div(
+    <.p("Spot for highlighting the trend of questions that have been the lowest rated in recent surveys...")
+)
+
+def confidenceAdvice(surveys:Seq[Assessment]) = <.div(
+    <.p("Spot for highlighting issues in confidence in recent questions...")
+)
