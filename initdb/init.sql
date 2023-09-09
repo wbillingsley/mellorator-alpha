481c0833a98fddb70519e4244321bfd56af75857
William Billingsley <wbillingsley@cantab.net>
2023-09-10 09:31:29 +1000

    Adds some columns for data to animal assessments table

--- initdb/init.sql
+++ initdb/init.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS melluser (
     id UUID,
     name VARCHAR NOT NULL,
     created BIGINT,
+    lastUpdate BIGINT,
     PRIMARY KEY(id)
 );
 
@@ -20,10 +21,12 @@ CREATE TABLE IF NOT EXISTS emails (
     melluser UUID,
     email VARCHAR NOT NULL UNIQUE,
     validationcode VARCHAR NOT NULL, -- a code to send out in email to verify it
+    timeSent BIGINT,
     validated BIGINT, -- whether / when the email address has been validated
-)
+    PRIMARY KEY(id)
+);
 
-create unique index email_lower on emails (lower(email))
+create unique index email_lower on emails (lower(email));
 
 -- Animals may belong to an organisation
 CREATE TABLE IF NOT EXISTS organization (
@@ -31,27 +34,37 @@ CREATE TABLE IF NOT EXISTS organization (
     name VARCHAR NOT NULL, 
     created BIGINT,
     PRIMARY KEY(id)
-)
+);
 
 -- Used for permission system
 CREATE TABLE IF NOT EXISTS organizationrole (
     melluser UUID,
     organization UUID,
-    orgrole VARCHAR NOT NULL, -- 
-
-)
+    roles VARCHAR ARRAY NOT NULL,
+    lastUpdate BIGINT,
+    CONSTRAINT fk_melluser FOREIGN KEY (melluser) REFERENCES melluser(id),
+    CONSTRAINT fk_organization FOREIGN KEY (organization) REFERENCES organization(id)
+);
 
 
 CREATE TABLE IF NOT EXISTS animal (
     id UUID,
     name VARCHAR NOT NULL,
-    kind VARCHAR,
+    species VARCHAR,
     melluser UUID,
     organisation UUID,
-    created BIGINT  
+    created BIGINT,
+    lastUpdate BIGINT,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS animalassessment (
     id UUID,
-    
-)
+    animal UUID,
+    score DOUBLE PRECISION ARRAY[16],
+    confidence DOUBLE PRECISION ARRAY[16],
+    clientTime BIGINT,
+    lastUpdate BIGINT,
+    CONSTRAINT fk_animal FOREIGN KEY (animal) REFERENCES animal(id),
+    PRIMARY KEY(id)
+);
